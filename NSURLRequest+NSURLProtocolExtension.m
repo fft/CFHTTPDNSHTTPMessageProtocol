190e6ffc69041f12ad71bf4c8a049044aad1abe2
fft <mailtocf@gmail.com>
2019-09-16 22:52:08 +0800

    Update NSURLRequest+NSURLProtocolExtension.m
    from https://segmentfault.com/a/1190000013991357

--- NSURLRequest+NSURLProtocolExtension.m
+++ NSURLRequest+NSURLProtocolExtension.m
@@ -16,19 +16,27 @@
     NSMutableURLRequest * req = [self mutableCopy];
     if ([self.HTTPMethod isEqualToString:@"POST"]) {
         if (!self.HTTPBody) {
-            uint8_t d[1024] = {0};
+            NSInteger maxLength = 1024;
+            uint8_t d[maxLength];
             NSInputStream *stream = self.HTTPBodyStream;
             NSMutableData *data = [[NSMutableData alloc] init];
             [stream open];
-            while ([stream hasBytesAvailable]) {
-                NSInteger len = [stream read:d maxLength:1024];
-                if (len > 0 && stream.streamError == nil) {
-                    [data appendBytes:(void *)d length:len];
+            BOOL endOfStreamReached = NO;
+            //不能用 [stream hasBytesAvailable]) 判断，处理图片文件的时候这里的[stream hasBytesAvailable]会始终返回YES，导致在while里面死循环。
+            while (!endOfStreamReached) {
+                NSInteger bytesRead = [stream read:d maxLength:maxLength];
+                if (bytesRead == 0) { //文件读取到最后
+                    endOfStreamReached = YES;
+                } else if (bytesRead == -1) { //文件读取错误
+                    endOfStreamReached = YES;
+                } else if (stream.streamError == nil) {
+                    [data appendBytes:(void *)d length:bytesRead];
                 }
             }
             req.HTTPBody = [data copy];
             [stream close];
         }
+        
     }
     return req;
 }
